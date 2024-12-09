74ad6cac2bc13a2d5ce909f7c64f9010922c662f
Yujia <liuyujia990604@gmail.com>
2024-12-09 23:52:39 +0800

    Update utils.py to improve session handling by removing print statements and adding sidebar notifications for read success and stage ID

--- utils.py
+++ utils.py
@@ -147,23 +147,19 @@ def configure_user_session():
         conn = st.connection("gsheets", type=GSheetsConnection)
         print("0: ", session_id)
         try:
-            print("0.1: ", session_id)
             df = conn.read(worksheet=session_id)
+            st.sidebar.write(f"read success")
         except:
             pass
         st.session_state["df"] = df
         df = st.session_state["df"]
         print(df)
         st.session_state["stage_id"] = df.iloc[-1]["stage_id"]
-        print("1: ", df.iloc[-1]["stage_id"])
+        st.sidebar.write(f"stage_id:{df.iloc[-1]['stage_id']}")
         st.session_state["state_ids"] = df.iloc[-1]["state_ids"]
-        print("2: ", st.session_state["state_ids"])
         st.session_state["student_type"] = df.iloc[-1]["student_type"]
-        print("3: ", df.iloc[-1]["student_type"])
         st.session_state["urge_state_id"] = df.iloc[-1]["urge_state_id"]
-        print("4: ", df.iloc[-1]["urge_state_id"])
         st.session_state["best_strategy_id"] = df.iloc[-1]["best_strategy_id"]
-        print("5: ", df.iloc[-1]["best_strategy_id"])
         st.session_state["messages"] = [{"role": df.iloc[i]["role"], "content": df.iloc[i]["content"]} for i in range(len(df))]
 
     st.sidebar.write(f"Current Session ID: {session_id}")
